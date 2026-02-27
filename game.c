7e623f7d9037633da068399cee2f890055f9ceba
jordan! <jordanbia05@gmail.com>
2026-02-27 13:36:20 -0700

    Added variables for all stats

--- game.c
+++ game.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 
+//initializing stat variables
+struct person {
+  char name[20];
+  int health;
+  int isAlive;
+};
+struct gameState {
+  //party
+  struct person party[5];
+  //resources
+  int food;
+  int oxen;
+  int money;
+  int ammo;
+  //spare parts
+  int wheels;
+  int axles;
+  int tongues;
+  //progress
+  int milesTraveled
+  int day;
+  int month;
+};
+  
 int main() {
 
 }
