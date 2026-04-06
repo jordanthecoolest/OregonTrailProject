5250fdb47b3a2f7b902d0f335f25429194cf52e9
jordan! <jordanbia05@gmail.com>
2026-04-06 10:06:46 -0700

    Update oregon_trail.h

--- oregon_trail.h
+++ oregon_trail.h
@@ -1,32 +1,13 @@
 #ifndef OREGON_TRAIL_H
 #define OREGON_TRAIL_H
-//initializing stat variables
-struct person {
-  char name[20];
-  int health;
-  int isAlive;
-};
-struct gameState {
-  //party
-  struct person party[5];
-  int job;
-  //resources
-  int food;
-  int oxen;
-  float money;
-  int ammo;
-  int medicine;
-  //spare parts
-  int wheels;
-  int axles;
-  int tongues;
-  int parts;
-  //progress
-  int milesTraveled;
-  int day;
-  int month;
-};
-//defining colors to keep code clean
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+//colors
 #define RED     "\x1b[31m"
 #define GREEN   "\x1b[32m"
 #define YELLOW  "\x1b[33m"
@@ -34,3 +15,59 @@ struct gameState {
 #define MAGENTA "\x1b[35m"
 #define CYAN    "\x1b[36m"
 #define RESET   "\x1b[0m"
+
+struct person {
+    char name[20];
+    int health;
+    int isAlive;
+};
+
+struct gameState {
+    struct person party[5];
+    int job;
+    
+    //resources
+    int food;
+    int oxen;
+    float money;
+    int ammo;
+    int medicine;
+    int clothing;
+    
+    //parts
+    int wheels;
+    int axles;
+    int tongues;
+    int parts;
+    
+    //progress
+    int milesTraveled;
+    int day;
+    int month;
+};
+
+//logic & stats
+int aliveCount(struct gameState *game);
+void nextDay(struct gameState *game);
+void changePartyHealth(struct gameState *game, int amount);
+
+//menus & travel
+void travelLoop(struct gameState *game);
+int fortMenu(struct gameState *game);
+int landmarkMenu(struct gameState *game);
+int checkStops(struct gameState *game, int oldMiles, int newMiles);
+int isFort(int miles);
+int isLandmark(int miles);
+
+//events
+void shop(struct gameState *game);
+void hunt(struct gameState *game);
+void cross_river(struct gameState *game);
+void restStop(struct gameState *game);
+void sink_wagon(struct gameState *game);
+
+//end
+void winScreen(struct gameState *game);
+void saveScore(struct gameState *game, int finalScore);
+
+#endif
